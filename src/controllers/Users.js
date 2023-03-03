9f37a323f7fc23973ea6d02d25fbd376b37a072a
lucas-palmeida <lucaspacheco95@gmail.com>
2023-03-03 13:13:30 -0300

    fix: corrige retorno de status e objeto no get da rota users/me

--- src/controllers/Users.js
+++ src/controllers/Users.js
@@ -23,7 +23,7 @@ export async function getUser(req, res) {
 
         requestedUser.rows[0].shortenedUrls = checkUrls.rows;
 
-        return res.sendStatus(requestedUser.rows[0]);
+        return res.status(200).send(requestedUser.rows[0]);
     } catch (error) {
         return res.status(500).send(error.message);
     }
